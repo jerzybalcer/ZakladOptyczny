76594cc3c260f023ebd64b1aee121f449d10de55
Jan Staniszewski <56159109+JanStaniszewski@users.noreply.github.com>
2022-01-08 13:25:55 +0100

    profile css update, new card visits

--- ZakladOptyczny/ZakladOptyczny/Controllers/HomeController.cs
+++ ZakladOptyczny/ZakladOptyczny/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace ZakladOptyczny.Controllers
             //var users = db.GetUsers();
             //db.AddTestUser();
 
-            return View("profile");
+            return View("visits");
         }
 
         public IActionResult Privacy()
