84238685055b68b0145efc87bba3e48c4e990f44
danikar208 <danikar@student.polsl.pl>
2022-01-08 16:14:52 +0100

    -enabled razor pgs -code clean up -fixed starting razor page and index folder -better labels

--- ZakladOptyczny/ZakladOptyczny/Program.cs
+++ ZakladOptyczny/ZakladOptyczny/Program.cs
@@ -19,6 +19,13 @@ builder.Services.AddDbContext<DatabaseContext>(options =>
 builder.Services.AddScoped<IAppointmentManager, AppointmentManager>();
 builder.Services.AddScoped<IUsersManager, UsersManager>();
 
+builder.Services.AddRazorPages();
+builder.Services.AddRazorPages().WithRazorPagesRoot("/Views/Home");
+builder.Services.AddMvc().AddRazorPagesOptions(options =>
+{
+    options.Conventions.AddPageRoute("/login", "");
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -36,9 +43,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
 
